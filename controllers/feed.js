ba4130a767e7f3a93d9be00d370b770859826d8c
K Kasi <you@example.com>
2020-03-19 00:59:08 +0530

    first commit

---
+++ controllers/feed.js
@@ -0,0 +1,20 @@
+let jsonData = require('./data.json')
+
+exports.getPosts = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(200).json({
+    posts: jsonData
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
+};
